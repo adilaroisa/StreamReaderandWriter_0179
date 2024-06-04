4254275e713f04c078af8cb28379ea854c60d37a
adilaroisa <adilaroisa688@gmail.com>
2024-06-04 10:44:30 +0700

    unlimited loop untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -15,6 +15,20 @@ int main()
     ofstream outfile;
     // menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "- ";
+        // menda[atkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memaskan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
 
 
