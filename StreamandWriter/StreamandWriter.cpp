ca109c249ab95b58de5a9a737fd0984fc627231f
adilaroisa <adilaroisa688@gmail.com>
2024-06-04 10:14:57 +0700

    membuat unlimited loop untuk menulis

--- StreamandWriter/StreamandWriter.cpp
+++ StreamandWriter/StreamandWriter.cpp
@@ -13,5 +13,18 @@ int main()
 	// menunjuk ke sebuah nama file
 	outfile.open("contohfile.txt");
 
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	// unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// loop akan berhasil jika anda memasukan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
 }
 
