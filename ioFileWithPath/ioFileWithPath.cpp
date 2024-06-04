673b1dbf5f4a5e3c47209290d2f02e0705f47043
adilaroisa <adilaroisa688@gmail.com>
2024-06-04 10:49:51 +0700

    melanjutkan membuat iofilewithpath

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -31,6 +31,29 @@ int main()
     }
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
+
+    // membuka file dalalm moode membaca
+    ifstream infile;
+
+    // menunjuk ke sebuah file 
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak dtemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
 
 
